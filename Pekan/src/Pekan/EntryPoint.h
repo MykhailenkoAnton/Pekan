53df71c2b1093f6fddc96274b3d1a637e80d552a
AntonMykhailenko <mykhailencko2594@gmail.com>
2025-10-15 11:50:46 +0200

    Premake was integrated.

--- Pekan/src/Pekan/EntryPoint.h
+++ Pekan/src/Pekan/EntryPoint.h
@@ -6,6 +6,10 @@ extern Pekan::Application* Pekan::CreateApplication();
 
 int main(int argc, char** argv)
 {	
+
+	Pekan::Log::Init();
+	PK_CORE_WARN("Core");
+	PK_INFO("App");
 	auto app = Pekan::CreateApplication();
 	app->Run();
 	delete app;
---
+++ premake5.lua
@@ -0,0 +1,116 @@
+workspace "Pekan"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Pekan"
+	location "Pekan"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PK_PLARFORM_WINDOWS",
+			"PK_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			"{MKDIR} ../bin/" .. outputdir .. "/Sandbox",
+			--("{COPYFILE} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+			("{COPYFILE} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox/")
+		}
+
+	filter "configurations:Debug"
+		defines "PK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PK_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PK_DIST"
+		optimize "On"
+
+	filter "system:windows"
+		buildoptions {"/utf-8"}
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Pekan/vendor/spdlog/include",
+		"Pekan/src"
+	}
+
+	links
+	{
+		"Pekan"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		
+
+		defines
+		{
+			"PK_PLARFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PK_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PK_DIST"
+		optimize "On"
+
+	filter "system:windows"
+		buildoptions { "/utf-8" }
